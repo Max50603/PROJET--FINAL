7d45129883ed83043a8bca9b9c2ca4f26cd16c6a
Max50603 <maxime.jarry@orange.fr>
2023-09-11 10:17:55 +0200

    dd

--- Les Nuages/lesnuages.js
+++ Les Nuages/lesnuages.js
@@ -56,6 +56,7 @@ texte1.style.display = "flex";
 texte1.style.justifyContent = "center";
 texte1.style.alignItems = "center";
 texte1.style.color = "white";
+texte1.style.fontSize = "1.4em";
 
 
 // SECTION MECANISME
@@ -75,9 +76,128 @@ texte2.style.margin = "10px";
 
 let petittitre = document.querySelector('.petittitre');
 petittitre.style.backgroundColor = "#31489f";
+petittitre.style.color = "white";
 // console.log(petittitre);
 
 
 // SECTION FORMATION
 let formation = document.querySelector('.flex2');
-console.log(formation);
+// console.log(formation);
+formation.style.display = "flex";
+formation.style.justifyContent = "center";
+formation.style.alignItems = "center";
+formation.style.border = "1px solid grey";
+formation.style.borderRadius = "10px";
+formation.style.boxShadow = "5px 5px 5px black";
+formation.style.margin = "10px";
+
+let titre4 = document.querySelector('.petittitre1');
+titre4.style.width = "100%";
+titre4.style.height = "auto";
+titre4.style.backgroundColor = "#31489f";
+titre4.style.display = "flex";
+titre4.style.justifyContent = "center";
+titre4.style.alignItems = "center";
+titre4.style.fontSize = "1.4em";
+titre4.style.color = "white";
+
+let left1 = document.querySelector('.left1');
+left1.style.width = "500px";
+left1.style.height = "500px";
+left1.style.margin = "10px";
+
+let texte3 = document.querySelector('.texte3');
+texte3.style.fontSize = "1.4em";
+texte3.style.margin = "10px";
+
+let flex3 = document.querySelector('.flex3');
+flex3.style.display = "flex";
+flex3.style.justifyContent = "center";
+flex3.style.alignItems = "center";
+flex3.style.border = "1px solid grey";
+flex3.style.borderRadius = "10px";
+flex3.style.boxShadow = "5px 5px 5px black";
+flex3.style.margin = "10px";
+
+let titre5 = document.querySelector('.petittitre2');
+titre5.style.width = "100%";
+titre5.style.height = "auto";
+titre5.style.backgroundColor = "#31489f";
+titre5.style.display = "flex";
+titre5.style.justifyContent = "center";
+titre5.style.alignItems = "center";
+titre5.style.fontSize = "1.4em";
+titre5.style.color = "white";
+
+let texte4 = document.querySelector('.texte4');
+texte4.style.fontSize = "1.4em";
+texte4.style.margin = "10px";
+
+let right = document.querySelector('.right');
+right.style.width = "500px";
+right.style.heigth = "500px";
+right.style.margin = "10px";
+
+let titre6 = document.querySelector('.petittitre3');
+titre6.style.width = "100%";
+titre6.style.height = "auto";
+titre6.style.backgroundColor = "#31489f";
+titre6.style.display = "flex";
+titre6.style.justifyContent = "center";
+titre6.style.alignItems = "center";
+titre6.style.fontSize = "1.4em";
+titre6.style.color = "white";
+
+let flex4 = document.querySelector('.flex4')
+flex4.style.display = "flex";
+flex4.style.justifyContent = "center";
+flex4.style.alignItems = "center";
+flex4.style.border = "1px solid grey";
+flex4.style.borderRadius = "10px";
+flex4.style.boxShadow = "5px 5px 5px black";
+flex4.style.margin = "10px";
+
+let image5001 = document.querySelector('.left2');
+image5001.style.width = "500px";
+image5001.style.height = "500px";
+image5001.style.margin = "10px";
+
+let texte5 = document.querySelector('.texte5');
+texte5.style.fontSize = "1.4em";
+texte5.style.margin = "10px";
+
+
+// SECTION TYPES
+let titre7 = document.querySelector('.titre7');
+titre7.style.width = "100%";
+titre7.style.height = "auto";
+titre7.style.backgroundColor = "#31489f";
+titre7.style.display = "flex";
+titre7.style.justifyContent = "center";
+titre7.style.alignItems = "center";
+titre7.style.fontSize = "1.4em";
+titre7.style.marginTop = "20px";
+
+let bleu = document.querySelector('.bleu');
+bleu.style.backgroundColor = "#31489f";
+bleu.style.color = "white";
+
+let flex5 = document.querySelector('.flex5');
+flex5.style.display = "flex";
+flex5.style.justifyContent = "center";
+flex5.style.alignItems = "center";
+flex5.style.border = "1px solid grey";
+flex5.style.borderRadius = "10px";
+flex5.style.boxShadow = "5px 5px 5px black";
+flex5.style.margin = "10px";
+
+let texte6 = document.querySelector('.texte6');
+texte6.style.fontSize = "1.4em";
+texte6.style.margin = "10px";
+
+let texte7 = document.querySelector('.texte7');
+texte7.style.fontSize = "1.4em";
+texte7.style.margin = "10px";
+
+
+// SECTION NUAGE
