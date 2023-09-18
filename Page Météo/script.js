284f65d681d8fcfe26659bf0ae26f47df02ea25b
Max50603 <maxime.jarry@orange.fr>
2023-09-18 14:05:55 +0200

    api météo fonctionnelle

--- Page Météo/script.js
+++ Page Météo/script.js
@@ -4,11 +4,15 @@ const searchBox = document.querySelector("input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `$appid=${apiKey}`);
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    // console.log(response);
+    var data = await response.json();
+    console.log(data);
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     }else{
+        
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "images/clouds.png";
         }else if(data.weather[0].main == "Clear"){
@@ -23,16 +27,13 @@ async function checkWeather(city){
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
     }
-    var data = await response.json();
 
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round() .data.main.temp + "°C";
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-    searchBtn.addEventListener("click", ()=>{
-        checkWeather(searchBox.value);
-    });
-        
 };
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
