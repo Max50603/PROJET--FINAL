7f17a7b5c1ac810fde4f84013ba95a8dade09376
Max50603 <maxime.jarry@orange.fr>
2023-10-06 15:51:41 +0200

    modif page Meteo General

--- Météorologie Générale/script.js
+++ Météorologie Générale/script.js
@@ -1,4 +1,4 @@
-//  BOUTON SMOOTH REMONTER HAUT
+//!  BOUTON SMOOTH REMONTER HAUT
 const btn = document.querySelector('.scroll');
 btn.addEventListener('click', () => {
     window.scrollTo({
@@ -6,4 +6,26 @@ btn.addEventListener('click', () => {
         left: 0,
         behavior: "smooth"
     });
+})
+
+//! SCROLLBAR
+window.onscroll = function() {myFunction()};
+function myFunction(){
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//! POUR AFFICHER DU TEXTE AVEC LE BOUTON
+const afficher = document.querySelector('.afficher');
+const reponse = document.querySelector('#reponse');
+reponse.style.display = "none";
+// ************************
+afficher.addEventListener('click', () => {
+    if(reponse.style.display == "block"){
+        reponse.style.display = "none";
+    }else{
+        reponse.style.display = "block";
+    }
 })
--- Page Principale/script.js
+++ Page Principale/script.js
@@ -8,3 +8,12 @@ btn.addEventListener('click', () => {
     });
 })
 
+
+window.onscroll = function() {myFunction()};
+function myFunction(){
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
