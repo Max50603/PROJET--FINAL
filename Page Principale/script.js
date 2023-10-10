a96a8a592b9e8fa84103cb6d1f7178bdd07abd27
Max50603 <maxime.jarry@orange.fr>
2023-10-10 23:05:51 +0200

    new animations page principale

--- Page Principale/script.js
+++ Page Principale/script.js
@@ -1,4 +1,4 @@
-//  BOUTON SMOOTH REMONTER HAUT
+//!  BOUTON SMOOTH REMONTER HAUT
 const btn = document.querySelector('.scroll');
 btn.addEventListener('click', () => {
     window.scrollTo({
@@ -8,7 +8,7 @@ btn.addEventListener('click', () => {
     });
 })
 
-
+//! SCROLLBAR
 window.onscroll = function() {myFunction()};
 function myFunction(){
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -17,3 +17,17 @@ function myFunction(){
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+//! BOUTON EN SAVOIR PLUS QUI AFFICHE DU TEXTE EN PLUS
+const texte = document.querySelector('.cv');
+const enplus = document.querySelector('.texteplus');
+let statut = true;
+texte.addEventListener('click', () =>{
+    if(statut){
+        enplus.style.display = "block";
+        enplus.style.animation = "slide 0.8s forwards";
+        statut = false;
+    }else{
+        enplus.style.display = "none";
+        statut = true;
+    }
+})
