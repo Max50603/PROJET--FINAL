739003f8e068dfcf5552e3af273da0f8734e1088
Max50603 <maxime.jarry@orange.fr>
2023-10-11 18:52:12 +0200

    modif lienbar et header

--- Phénomènes Extrèmes/script.js
+++ Phénomènes Extrèmes/script.js
@@ -1,4 +1,4 @@
-//  BOUTON SMOOTH REMONTER HAUT
+//!  BOUTON SMOOTH REMONTER HAUT
 const btn = document.querySelector('.scroll');
 btn.addEventListener('click', () => {
     window.scrollTo({
@@ -7,3 +7,12 @@ btn.addEventListener('click', () => {
         behavior: "smooth"
     });
 })
+
+//! SCROLLBAR
+window.onscroll = function() {myFunction()};
+function myFunction(){
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
