ccf82f1a15207bac15efe0bcc9790994f19b760b
Max50603 <maxime.jarry@orange.fr>
2023-10-14 15:23:04 +0200

    dd

--- Page Météo/script.js
+++ Page Météo/script.js
@@ -12,7 +12,6 @@ async function checkWeather(city){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     }else{
-        
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "images/clouds.png";
         }else if(data.weather[0].main == "Clear"){
@@ -27,7 +26,6 @@ async function checkWeather(city){
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
     }
-
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = data.main.temp + "°C";
