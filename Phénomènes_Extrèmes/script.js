85225b19ba62c12a071dde9b72bd3c9944f4a222
Max50603 <maxime.jarry@orange.fr>
2023-10-21 17:00:30 +0200

    fin modif page Phéno. Extrem

--- Phénomènes_Extrèmes/script.js
+++ Phénomènes_Extrèmes/script.js
@@ -9,10 +9,10 @@ btn.addEventListener('click', () => {
 })
 
 //! SCROLLBAR
-window.onscroll = function() {myFunction()};
-function myFunction(){
-  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// window.onscroll = function() {myFunction()};
+// function myFunction(){
+//   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   let scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
